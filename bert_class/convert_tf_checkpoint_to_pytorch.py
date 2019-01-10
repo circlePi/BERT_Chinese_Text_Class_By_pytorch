c9cbd9e173be9a3ffb31f7f85d48266ab6eab2ef
circlePi <32888701+circlePi@users.noreply.github.com>
2019-01-10 16:21:30 +0800

    Update convert_tf_checkpoint_to_pytorch.py

--- bert_class/convert_tf_checkpoint_to_pytorch.py
+++ bert_class/convert_tf_checkpoint_to_pytorch.py
@@ -2,7 +2,7 @@ from pytorch_pretrained_bert.convert_tf_checkpoint_to_pytorch import convert_tf_
 
 
 if __name__ == "__main__":
-    tf_checkpoint_path = "/home/daizelin/bert_class/model/chinese_L-12_H-768_A-12/bert_model.ckpt.index"
+    tf_checkpoint_path = "/home/daizelin/bert_class/model/chinese_L-12_H-768_A-12/bert_model.ckpt"
     bert_config_file = "/home/daizelin/bert_class/model/chinese_L-12_H-768_A-12/bert_config.json"
     pytorch_dump_path = "/home/daizelin/bert_class/model/chinese_L-12_H-768_A-12.ckpt"
     convert_tf_checkpoint_to_pytorch(tf_checkpoint_path, bert_config_file, pytorch_dump_path)
